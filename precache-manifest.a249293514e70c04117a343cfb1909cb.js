6787a29393bd0594f0c18888ec8733dbe873eb47
Redoni24 <redonemini18@gmail.com>
2020-04-30 22:00:23 +0200

    Updates

---
+++ precache-manifest.a249293514e70c04117a343cfb1909cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e0a17327891173944f14fdaf089d23",
+    "url": "/React-Production-Build/index.html"
+  },
+  {
+    "revision": "e0239025457c1bd54506",
+    "url": "/React-Production-Build/static/js/2.703ba6c7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/React-Production-Build/static/js/2.703ba6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08542fdf414b2b338559",
+    "url": "/React-Production-Build/static/js/main.ee53df2b.chunk.js"
+  },
+  {
+    "revision": "d39a98591c03772b1ef5",
+    "url": "/React-Production-Build/static/js/runtime-main.0af97b1c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/React-Production-Build/static/media/image.d7265326.png"
+  }
+]);
